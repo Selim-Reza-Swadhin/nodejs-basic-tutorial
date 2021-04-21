f8fe25c4d8d28f1ad4821e6e8a314c64d22255b7
Selim Reza Swadhin <selimrezaswadhim@gmail.com>
2021-04-21 07:30:21 +0600

    add delete, select, update data for nodejs

--- nodejs_mysql_conn/index.js
+++ nodejs_mysql_conn/index.js
@@ -17,19 +17,61 @@ conn.connect(function(err) {
         console.log("Connection failed");
     } else {
         console.log("Connection Success");
-        InsertData(conn);
+        //InsertData(conn);
+        //DeleteData(conn);
+        //UpdateData(conn);
+        SelectData(conn);
     }
 });
 
 
 //insert data
 function InsertData(conn) {
-    let sql_query = "INSERT INTO `students_list`(`name`, `roll`, `class`, `phone`, `city`) VALUES ('selim','12','nine','01724063642','natore')";
+    let sql_query = "INSERT INTO `students_list`(`name`, `roll`, `class`, `phone`, `city`) VALUES ('swadhin','15','nine','01724063643','natore')";
     conn.query(sql_query, function(err) {
         if (err) {
             console.log("Data not inserted");
         } else {
             console.log("Data inserted success");
         }
+    });
+}
+
+//delete data
+function DeleteData(conn) {
+    let sql_query = "DELETE FROM `students_list` WHERE id= '4'";
+    conn.query(sql_query, function(err) {
+        if (err) {
+            console.log("Data not deleted");
+        } else {
+            console.log("Data deleted success");
+        }
+    });
+}
+
+//update data
+function UpdateData(conn) {
+    let sql_query = "UPDATE `students_list` SET `class` = '10', `phone` = '01710392318', `city` = 'dhaka' WHERE id = '3'";
+    conn.query(sql_query, function(err) {
+        if (err) {
+            console.log("Data not updated");
+        } else {
+            console.log("Data update success");
+        }
+    });
+}
+
+//select data
+function SelectData(conn) {
+    let sql_query = "SELECT * FROM `students_list`";
+    conn.query(sql_query, function(err, result) {
+        // if (err) {
+        //     console.log("Data not selected");
+        // } else {
+        //     console.log(result);
+        // }
+
+        if (err) throw err;
+        console.log(result);
     });
 }
